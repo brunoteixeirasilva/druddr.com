ab7ed8e7ae8cdbafac16a889f2eb2c9c787ebff3
lologrignola <grignolabusiness@gmail.com>
2022-03-24 20:08:21 -0300

    Conversion of Class Components to Function Components + Bugfixes + version 1.0.3

---
+++ CHANGELOG.md
@@ -0,0 +1,13 @@
+# Druddr Website
+
+## Changelog
+
+### 24/03/2022 - React Session #1
+
+#### Changes applied
+
+-   HeaderContainer.js: Conversion from class to Function component.
+-   Contact.js: Conversion from class to Function component.
+-   Minor bugfixes on the existing components, after conversion.
+-   [WIP] Problem with Slide component not behaving properly (isolated).
+-   Closed release version 1.0.3
--- src/components/scene/contact/Contact.js
+++ src/components/scene/contact/Contact.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
 import routes from '../../../utils/routes/routes'
 import SceneRoot from '../root'
 import {
@@ -8,7 +9,7 @@ import {
 	TextField,
 	Slide,
 	Typography,
-	Button
+	Button,
 } from '@material-ui/core'
 import LocalPhoneIcon from '@material-ui/icons/LocalPhoneRounded'
 import VideocamIcon from '@material-ui/icons/VideocamRounded'
@@ -23,9 +24,168 @@ import { translate } from '../../../utils/lang'
  * @author brunoteixeirasilva
  * @version 1.0
  */
-class ContactScene extends React.PureComponent {
+
+function Contact({ classes, match }) {
+	const [mounted, setMounted] = useState(false)
+
+	useEffect(() => {
+		setMounted(true)
+
+		return () => {
+			setMounted(false)
+		}
+	}, [])
+
+	return (
+		<SceneRoot color="secondary">
+			{/* <Slide
+				direction={'up'}
+				in={mounted}
+				// mountOnEnter
+				unmountOnExit
+				className={classNames(classes.slide, {
+					[classes.out]: !mounted,
+				})}
+			> */}
+			<React.Fragment>
+				<div className={classes.root}>
+					<Typography
+						className={classes.invertedTextColors}
+						component="section"
+						variant="h2"
+					>
+						<b>React</b> Developer
+					</Typography>
+					<Typography
+						className={classes.invertedTextColors}
+						component="section"
+						variant="body1"
+					>
+						JavaScript(ing) since <b>2008</b>
+					</Typography>
+					<TextField
+						className={classes.invertedTextColors}
+						disabled
+						variant="outlined"
+						label="Skype"
+						id="skype"
+						aria-label="skype"
+						value={translate('author/skype-address')}
+						InputProps={{
+							endAdornment: (
+								<InputAdornment position="end">
+									<IconButton
+										aria-label={translate(
+											'author/skype-call'
+										)}
+										onClick={(event) =>
+											(window.location.href = translate(
+												'author/skype-address-href'
+											))
+										}
+									>
+										<VideocamIcon />
+									</IconButton>
+								</InputAdornment>
+							),
+						}}
+					/>
+					<TextField
+						className={classes.invertedTextColors}
+						disabled
+						variant="outlined"
+						label="Tel"
+						id="tel"
+						aria-label="Telefone"
+						value={translate('author/phone-number')}
+						InputProps={{
+							endAdornment: (
+								<InputAdornment position="end">
+									<IconButton
+										aria-label={translate(
+											'author/phone-number-call'
+										)}
+										onClick={(event) =>
+											(window.location.href = translate(
+												'author/phone-number-href'
+											))
+										}
+									>
+										<LocalPhoneIcon />
+									</IconButton>
+								</InputAdornment>
+							),
+						}}
+					/>
+					<TextField
+						className={classes.invertedTextColors}
+						disabled
+						variant="outlined"
+						label="E-mail"
+						id="email"
+						aria-label="email"
+						value={translate('author/e-mail')}
+						InputProps={{
+							endAdornment: (
+								<InputAdornment position="end">
+									<IconButton
+										aria-label={translate(
+											'author/e-mail-call'
+										)}
+										onClick={(event) =>
+											(window.location.href =
+												translate('author/e-mail-href'))
+										}
+									>
+										<EmailIcon />
+									</IconButton>
+								</InputAdornment>
+							),
+						}}
+					/>
+					<Typography
+						className={classes.invertedTextColors}
+						component="section"
+						variant="body1"
+					>
+						<b>Powered</b> by
+						<Button
+							aria-label={translate('title/author')}
+							className={classNames(
+								classes.textButton,
+								classes.invertedTextColors
+							)}
+							variant="text"
+							component="b"
+							onClick={
+								(event) => false
+								// (window.location.href =
+								// 	'https://fb.me/bruno.teixeiradasilva')
+							}
+						>
+							{translate('title/author')}
+						</Button>
+					</Typography>
+				</div>
+			</React.Fragment>
+			{/* </Slide> */}
+			<CloseButton
+				buttonColor="primary"
+				onClick={async (event, history) => {
+					setMounted(false).then((s) => {
+						return history.push(routes.index)
+					})
+
+					return false
+				}}
+			/>
+		</SceneRoot>
+	)
+}
+
+/* class ContactScene extends React.PureComponent {
 	state = {
-		mounted: false
+		mounted: false,
 	}
 
 	async setMounted(mounted = false) {
@@ -56,7 +216,7 @@ class ContactScene extends React.PureComponent {
 					// mountOnEnter
 					unmountOnExit
 					className={classNames(classes.slide, {
-						[classes.out]: !self.state.mounted
+						[classes.out]: !self.state.mounted,
 					})}
 				>
 					<React.Fragment>
@@ -91,15 +251,16 @@ class ContactScene extends React.PureComponent {
 													'author/skype-call'
 												)}
 												onClick={(event) =>
-													(window.location.href = translate(
-														'author/skype-address-href'
-													))
+													(window.location.href =
+														translate(
+															'author/skype-address-href'
+														))
 												}
 											>
 												<VideocamIcon />
 											</IconButton>
 										</InputAdornment>
-									)
+									),
 								}}
 							/>
 							<TextField
@@ -118,15 +279,16 @@ class ContactScene extends React.PureComponent {
 													'author/phone-number-call'
 												)}
 												onClick={(event) =>
-													(window.location.href = translate(
-														'author/phone-number-href'
-													))
+													(window.location.href =
+														translate(
+															'author/phone-number-href'
+														))
 												}
 											>
 												<LocalPhoneIcon />
 											</IconButton>
 										</InputAdornment>
-									)
+									),
 								}}
 							/>
 							<TextField
@@ -145,15 +307,16 @@ class ContactScene extends React.PureComponent {
 													'author/e-mail-call'
 												)}
 												onClick={(event) =>
-													(window.location.href = translate(
-														'author/e-mail-href'
-													))
+													(window.location.href =
+														translate(
+															'author/e-mail-href'
+														))
 												}
 											>
 												<EmailIcon />
 											</IconButton>
 										</InputAdornment>
-									)
+									),
 								}}
 							/>
 							<Typography
@@ -195,8 +358,8 @@ class ContactScene extends React.PureComponent {
 			</SceneRoot>
 		)
 	}
-}
+} */
 
-ContactScene.propTypes = {}
+Contact.propTypes = {}
 
-export default withStyles(styles)(ContactScene)
+export default withStyles(styles)(Contact)
