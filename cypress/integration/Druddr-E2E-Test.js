2180581c0187177d84d773ff710c3f5ed212cdb0
lologrignola <grignolabusiness@gmail.com>
2022-04-05 19:16:57 -0300

    Update Druddr-E2E-Test.js

--- cypress/integration/Druddr-E2E-Test.js
+++ cypress/integration/Druddr-E2E-Test.js
@@ -3,18 +3,18 @@ import ComponentMap from '../../src/constants/componentMap'
 import router from '../../src/utils/routes/routes'
 
 describe('Druddr E2E Test', () => {
-	it('Visits the Druddr home page and checks if is correctly loaded', () => {
+	it('Visits Druddr home', () => {
 		cy.visit(router.url)
 		cy.contains(translate('title/app'))
 	})
 
-	it('Clicks bottom button and checks if its loaded', () => {
+	it('Clicks bottom right Play button', () => {
 		cy.get(`[data-cy=${ComponentMap.playButton}]`).click()
 		cy.url().should('include', router.contact)
 		cy.contains(translate('title/author'))
 	})
 
-	it('Tests if top right button works properly', () => {
+	it('Tests top right Instagram button', () => {
 		cy.get(`[data-cy=${ComponentMap.instagramButton}]`).trigger('mouseover')
 		cy.contains(translate('label/instagram'))
 		cy.get(`[data-cy=${ComponentMap.instagramButton}]`).click()
@@ -22,7 +22,7 @@ describe('Druddr E2E Test', () => {
 		cy.go('back')
 	})
 
-	it('Tests bottom right button', () => {
+	it('Tests bottom right Back button', () => {
 		cy.get(`[data-cy=${ComponentMap.closeButton}]`).click()
 		cy.contains(translate('label/we-build'))
 	})
