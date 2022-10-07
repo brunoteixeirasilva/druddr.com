f7ce8413763fd9eb0c1eb5888cdf5d36fd621c30
Bruno Teixeira da Silva <bruno.teixeira.silva@gmail.com>
2022-10-07 11:54:54 -0300

    React training #3
    Co-authored-by: Noelia Fernández Piñeyro <noueliah@users.noreply.github.com>
    Co-authored-by: lologrignola <lologrignola@users.noreply.github.com>
    Co-authored-by: amarpremchand <amarpremchand@users.noreply.github.com>
    Co-authored-by: Emmanuel <EmmaDwebber@users.noreply.github.com>

---
+++ src/components/modal/footer/ModalFooter.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function ModalFooter(props) {
+	const { controls, id, any } = props
+	const newProps = { controls, id, any }
+
+	return <></>
+}
+
+export default ModalFooter
